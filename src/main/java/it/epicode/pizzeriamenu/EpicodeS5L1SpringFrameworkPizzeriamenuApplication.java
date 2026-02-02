542d0a05c55d9f5d85d3b7bd7ac1fc25451d101b
Tanjin <NamelessKing@users.noreply.github.com>
2026-02-02 16:05:10 +0100

    first commit

---
+++ src/main/java/it/epicode/pizzeriamenu/EpicodeS5L1SpringFrameworkPizzeriamenuApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.pizzeriamenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeS5L1SpringFrameworkPizzeriamenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeS5L1SpringFrameworkPizzeriamenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/pizzeriamenu/EpicodeS5L1SpringFrameworkPizzeriamenuApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.pizzeriamenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeS5L1SpringFrameworkPizzeriamenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
